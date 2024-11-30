d4dee554cda2735f93df67d35e9b853b35968093
arjunchy <arjunchaudhary477058@gmail.com>
2024-11-30 18:46:21 +0530

    booking issue solved

--- backend/models/booking.js
+++ backend/models/booking.js
@@ -1,32 +1,32 @@
-// models/booking.js
-module.exports = (sequelize, DataTypes) => {
-  const Booking = sequelize.define('Booking', {
-      id: {
-          type: DataTypes.INTEGER,
-          primaryKey: true,
-          autoIncrement: true,
-      },
-      user_id: {
-          type: DataTypes.INTEGER,
-          allowNull: false,
-      },
-      vehicle_id: {
-          type: DataTypes.INTEGER,
-          references: {
-              model: 'Vehicles', // Ensure this matches your table name
-              key: 'id',
-          },
-          allowNull: false,
-      },
-      booking_date: {
-          type: DataTypes.DATE,
-          allowNull: false,
-      },
-      booking_type: { // Add this new field
-          type: DataTypes.STRING,
-          allowNull: false, // You can set this to true if you want it to be optional
-      },
-  }, {});
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
 
-  return Booking;
-};
+const Booking = sequelize.define('Booking', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    vehicle_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'Vehicles', // Ensure this matches your table name
+            key: 'id',
+        },
+        allowNull: false,
+    },
+    booking_date: {
+        type: DataTypes.DATE,
+        allowNull: false,
+    },
+    booking_type: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+}, {});
+
+module.exports = Booking; // Ensure this is exporting the Booking model correctly
